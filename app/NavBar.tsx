c54755fe3bf850a3277115c76fa9bce96d2c05a4
thanhNha <thanhna.k13@gmail.com>
2023-10-25 13:05:41 +0700

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,17 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const NavBar = () => {
+  /**
+   * hooks usePathname: lay duongdan tren URL
+   *  de xu ly active navbar
+   */
+  const currentPath = usePathname();
   const links = [
     { label: 'Dashboard', href: '/' },
     {
@@ -19,7 +28,12 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            // classnaems giup render classNames theo dieu kien
+            className={classNames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
             href={link.href}
           >
             {link.label}
