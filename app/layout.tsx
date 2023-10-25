a0707fbd66cc2bc3c82f340607081ed2b87f739d
thanhNha <thanhna.k13@gmail.com>
2023-10-25 15:12:10 +0700

    Build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className} suppressHydrationWarning>
         <Theme>
           <NavBar />
-          {children}
+          <main className='p-5'>{children}</main>
         </Theme>
       </body>
     </html>
